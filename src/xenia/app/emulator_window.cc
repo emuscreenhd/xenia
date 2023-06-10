031fd15dadb386f55491934bdbab0ab0b0f0588c
emuscreenhd <136181390+emuscreenhd@users.noreply.github.com>
2023-06-10 17:10:29 +0200

    Update emulator_window.cc

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -132,7 +132,7 @@ using xe::ui::KeyEvent;
 using xe::ui::MenuItem;
 using xe::ui::UIEvent;
 
-const std::string kBaseTitle = "Xenia";
+const std::string kBaseTitle = "Xenia By Backgamon";
 
 EmulatorWindow::EmulatorWindow(Emulator* emulator,
                                ui::WindowedAppContext& app_context)
